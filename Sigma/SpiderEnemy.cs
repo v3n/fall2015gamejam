c982a780f0003ec608ac6276506686f8e9945377
kalibcrone <kalibcrone@yahoo.com>
2015-09-26 20:57:06 -0400

    Corrected Spider Enemy Class

--- Sigma/SpiderEnemy.cs
+++ Sigma/SpiderEnemy.cs
@@ -13,26 +13,56 @@ namespace Sigma
     public class SpiderEnemy
     {
         Model model;
-        SpiderEnemy[] spiders;
+        SpiderEnemy[] spidersArr;
+        Vector3 position;
+        Vector2 heading;
+
+        int health;
+        int attackDistance;
+        float speed;
 
 
-        public void Initialize(ContentManager contMgr, Vector3 position, Vector2 heading)
+        public void Initialize(ContentManager contMgr, Vector3 pos, Vector2 head)
         {
             model = contMgr.Load<Model>("SpiderEnemy");
+            position = pos;
+            heading = head;
 
+            health = 100;
+            attackDistance = 20;
+            speed = 4;
         }
 
 
 
         public void Update(Vector3 playerPosition)
         {
-            move(playerPosition);
+            if ((Vector3.Distance(playerPosition, position) > attackDistance))
+            {
+                follow(playerPosition);
+            }
+            else
+            {
+                idle();
+            }
+
         }
 
-        public void move(Vector3 playerPos)
+        private void idle()
         {
-            
+            throw new NotImplementedException();
+            //idle animation
         }
+
+        public void follow(Vector3 playerPosition)
+        {
+            double angle = Math.Atan2(playerPosition.Y - position.Y, playerPosition.X - position.X); //Find angle of enemy to player
+            Vector3 velocity = new Vector3((float)Math.Cos(angle), (float)Math.Sin(angle), 0);
+            position += velocity * speed; //makes enemy follow player.  Increase/Decrease speed (float) to adjust movement speed.
+
+
+        }
+
         public void attack()
         {
             //do attack
